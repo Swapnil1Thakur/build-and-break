628784e000a94f06d8d1d9156fdfd7674c1d36e4
Swapnil Thakur <thakur7swapnil@gmail.com>
2026-08-11 18:13:25 +0530

    added some files

---
+++ src/main/java/com/swapnil/gold_loan_system/GoldLoanSystemApplication.java
@@ -0,0 +1,13 @@
+package com.swapnil.gold_loan_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldLoanSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldLoanSystemApplication.class, args);
+	}
+
+}
